1d4c86ea669f17557a0e689e6975c2d86b55a34a
shanye <shanye.xzq@alibaba-inc.com>
2025-03-14 10:39:29 +0800

    module: remove unused variables; examples/complete: improve the examples

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,29 @@
 output "this_ecs_id" {
-  value = alicloud_instance.default.id
+  description = "The ID of the ECS instance."
+  value       = alicloud_instance.default.id
 }
 
 output "this_redis_id" {
-  value = alicloud_kvstore_instance.default.id
+  description = "The ID of the Redis instance."
+  value       = alicloud_kvstore_instance.default.id
 }
 
 output "this_mongodb_id" {
-  value = alicloud_mongodb_instance.default.id
+  description = "The ID of the MongoDB instance."
+  value       = alicloud_mongodb_instance.default.id
 }
 
 output "this_polardb_cluster_id" {
-  value = alicloud_polardb_cluster.default.id
+  description = "The ID of the PolarDB cluster."
+  value       = alicloud_polardb_cluster.default.id
 }
 
 output "this_polardb_database_id" {
-  value = alicloud_polardb_database.default.id
+  description = "The ID of the PolarDB database."
+  value       = alicloud_polardb_database.default.id
 }
 
 output "this_ecs_name" {
-  value = alicloud_instance.default.instance_name
+  description = "The name of the ECS instance."
+  value       = alicloud_instance.default.instance_name
 }
